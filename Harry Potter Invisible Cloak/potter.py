ced70b208dd18b9b17b9754a132a1d551b17b4c1
afrazchelsea <afrazchelsea@gmail.com>
2019-09-04 02:13:48 +0530

    update

--- Harry Potter Invisible Cloak/potter.py
+++ Harry Potter Invisible Cloak/potter.py
@@ -10,22 +10,18 @@ import cv2
 import time
 
 
-print("!! Invisibility is no more a Dream !!")
+print("Stupefy!")
 
-# Capturing Webcam Feed
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) # Capturing Video through Webcam
 
-time.sleep(3)
-
-count = 0
+time.sleep(3) # Give time for Camera to settle and be stable 
 
 background = 0
 
-# Capturing Static Background Frame
 
 for i in range(60):
 
-    ret, background = cap.read()
+    ret, background = cap.read() # Capturing Static Background
 
 
 # Flip the Image
@@ -39,35 +35,33 @@ while(cap.isOpened()):
     if not ret:
         break
 
-    count+=1
 
     img = np.flip(img, axis=1)
     
-#Converting from BGR to HSV
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    lower_red = np.array([0, 120, 70])
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # Converting from BGR to HSV
+    
+    lower_red = np.array([0, 120, 70]) # HSV Values Red= 0-10,170-180
     upper_red = np.array([10, 255, 255])
-    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+    mask1 = cv2.inRange(hsv, lower_red, upper_red) # Separating the cloak part
 
     lower_red = np.array([170, 120, 70])
     upper_red = np.array([180, 255, 255])
-    mask2 = cv2.inRange(hsv, lower_red, upper_red)
+    mask2 = cv2.inRange(hsv, lower_red, upper_red) # Everything except the cloak
     
-    mask1 = mask1 + mask2
+    mask1 = mask1 + mask2 # Combining both masks 
 
-    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8), iterations=2)
-    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8), iterations=1)
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8), iterations=2) # MORPH_OPEN removes noise
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8), iterations=1) # For smoothness
 
-    mask2 = cv2.bitwise_not(mask1)
+    mask2 = cv2.bitwise_not(mask1) # Separate the cloak from the background
 
-    res1 = cv2.bitwise_and(background, background, mask = mask1)
-    res2 = cv2.bitwise_and(img, img, mask = mask2)
-    final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
+    res1 = cv2.bitwise_and(background, background, mask = mask1) # Used for segmentation of color
+    res2 = cv2.bitwise_and(img, img, mask = mask2) # Used to substitute the cloak part (we super impose the two images)
+    final_output = cv2.addWeighted(res1, 1, res2, 1, 0) # Combining results
 
-    cv2.imshow('Eureka !!', final_output)
+    cv2.imshow("You're a wizard, Harry!", final_output)
     k = cv2.waitKey(10)
-    if k == 27:
+    if k == 27:   # Triggered when escape-key is pressed and window shuts down
         break
 
 cap.release()
